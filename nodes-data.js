dd1d9eb5d5d5226e96a9e7b1d354ca794f0522a5
jyol <joannaliu19@gmail.com>
2026-03-10 00:05:46 +0900

    added links and fixed the about button

--- nodes-data.js
+++ nodes-data.js
@@ -4,7 +4,9 @@ const NODES = [
         city: 'Shanghai',
         tz: 'UTC+8',
         org: 'rect*repair',
-        url: 'http://instagram.com/rect_repair',
+        url: 'http://instagram.com/rect_repair', // host/org URL
+        cityUrl: 'https://www.xiaohongshu.com/explore/69ac2bb1000000001a032d64?xsec_token=ABhD0hqrJ-mYjsKYxP87S8HoXucS-fg4w7yoKaax95dAg=&xsec_source=pc_user',
+        schedule: '2 day game jam (21 – 22 March)<br>+ playtest weekend (28 – 29th March)',
         coords: [121.4737, 31.2304]
     },
     {
@@ -13,6 +15,8 @@ const NODES = [
         tz: 'UTC+9',
         org: 'gaemz',
         url: 'http://gaemz.carrd.co/',
+        cityUrl: 'https://forms.gle/nVx7nyXKbzCUUH6P6/',
+        schedule: '1 day game jam (21st March)<br>+ playtest session (28th March)',
         coords: [139.6917, 35.6895]
     },
     {
@@ -21,6 +25,8 @@ const NODES = [
         tz: 'UTC+0',
         org: 'playfool',
         url: 'https://studioplayfool.com/',
+        cityUrl: 'https://docs.google.com/forms/d/1lv-ESsW2MV-v_DeBV9iEeRU6QCoYgGK825uNox-lGkE/edit?usp=drive_web&ouid=117430152361032132649',
+        schedule: '1 day game jam (21st March)<br>+ playtest session (28th March)',
         coords: [-0.1276, 51.5074]
     },
     {
@@ -29,6 +35,8 @@ const NODES = [
         tz: 'UTC+8',
         org: 'ferran',
         url: 'https://fonserbc.itch.io/',
+        cityUrl: 'https://forms.gle/TkA9NmeXpgURE1Lq7',
+        schedule: '1 day game jam (21st March)<br>+ playtest session (28th March)',
         coords: [121.5654, 25.0330]
     },
     {
@@ -37,6 +45,8 @@ const NODES = [
         tz: 'UTC+1',
         org: 'jana romanova',
         url: 'https://janaromanova.com/',
+        cityUrl: 'https://forms.gle/EeX4rqXeeQfhwNuQ9',
+        schedule: '1 day game jam (21st March)<br>+ playtest session (29th March or later)',
         coords: [4.3007, 52.0705]
     },
     {
@@ -45,6 +55,8 @@ const NODES = [
         tz: 'UTC-6',
         org: 'TBD',
         url: null,
+        cityUrl: null,
+        schedule: null,
         coords: [-99.1332, 19.4326]
     },
     {
@@ -53,6 +65,8 @@ const NODES = [
         tz: 'pending',
         org: 'TBD',
         url: null,
+        cityUrl: null,
+        schedule: null,
         coords: [-74.0060, 40.7128],
         pending: true
     }
